48889a457394cdbb286793eab05c7e8e18fb0020
anivesh <anivessamal26@gmail.com>
2025-08-28 15:17:20 +0530

    Initial commit - Fullstack URL Shortener

---
+++ url-shortener-react/src/components/Footer.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin, FaLaptopCode } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-custom-gradient text-white py-8 z-40 relative">
+      <div className="container mx-auto px-6 lg:px-14 flex flex-col lg:flex-row lg:justify-between items-center gap-4">
+        <div className="text-center lg:text-left">
+          <h2 className="text-3xl font-bold mb-2">Linklytics</h2>
+          <p>Simplifying URL shortening for efficient sharing</p>
+        </div>
+
+        <p className="mt-4 lg:mt-0">
+          &copy; 2025 Linklytics. All rights reserved.
+        </p>
+
+        <div className="flex space-x-6 mt-4 lg:mt-0">
+         
+          <a href="https://www.instagram.com/an1vesh?igsh=dHRrNnZ6emE2anRi" className="hover:text-gray-200">
+            <FaInstagram size={24} />
+          </a>
+           <a href="https://github.com/aniveshsamal" className="hover:text-gray-200">
+            <FaGithub size={24} />
+          </a>
+          <a href="https://leetcode.com/u/Anivesh_Samal/" className="hover:text-gray-200">
+            <SiLeetcode size={24} />
+          </a>
+          <a href="https://www.linkedin.com/in/anivesh-samal/" className="hover:text-gray-200">
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+      </div>
+       {/* Developer Badge */}
+ <div className="absolute bottom-2 right-6 flex items-center gap-1 text-white text-xs font-mono shadow-lg px-2 py-1 rounded-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 animate-bounce hover:scale-110 transform transition-all duration-500">
+    <FaLaptopCode size={16} className="text-yellow-300" />
+
+    <span className="font-semibold text-white tracking-wide flex">
+      {"Dev by".split("").map((char, i) => (
+        <span
+          key={i}
+          className="inline-block"
+          style={{
+            transformOrigin: "bottom center",
+            animation: `tilt 1s ease-in-out ${i * 0.1}s infinite`,
+          }}
+        >
+          {char}
+        </span>
+      ))}
+    </span>
+
+    <a
+      href="https://your-portfolio-link.com"
+      target="_blank"
+      className="underline decoration-dashed decoration-yellow-300 hover:text-yellow-200 font-medium ml-1 flex"
+    >
+      {"Anivesh".split("").map((char, i) => (
+        <span
+          key={i}
+          className="inline-block"
+          style={{
+            transformOrigin: "bottom center",
+            animation: `tilt 1s ease-in-out ${0.5 + i * 0.1}s infinite`,
+          }}
+        >
+          {char}
+        </span>
+      ))}
+      &nbsp;
+      {"Samal".split("").map((char, i) => (
+        <span
+          key={i}
+          className="inline-block"
+          style={{
+            transformOrigin: "bottom center",
+            animation: `tilt 1s ease-in-out ${1 + i * 0.1}s infinite`,
+          }}
+        >
+          {char}
+        </span>
+      ))}
+    </a>
+  </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ url-shortener-react/src/hooks/useQuery.js
@@ -0,0 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../api/api";
+
+export const useFetchMyShortUrls = (token, onError) => {
+  return useQuery({
+    queryKey: ["my-shortenurls", token],
+    queryFn: async () => {
+      const res = await api.get("/api/urls/myurls", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res;
+    },
+    select: (res) => {
+      const sortedData = (res.data || []).sort(
+        (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+      );
+      return sortedData;
+    },
+    onError,
+    staleTime: 5000,
+  });
+};
+
+export const useFetchTotalClicks = (token, onError) => {
+  return useQuery({
+    queryKey: ["url-totalclick", token],
+    queryFn: async () => {
+      const res = await api.get(
+        "/api/urls/totalClicks?startDate=2024-01-01&endDate=2025-12-31",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return res;
+    },
+    select: (res) => {
+      const convertToArray = Object.keys(res.data || {}).map((key) => ({
+        clickDate: key,
+        count: res.data[key],
+      }));
+      return convertToArray;
+    },
+    onError,
+    staleTime: 5000,
+  });
+};
